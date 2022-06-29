8c06056a323aa49361a1fb558a341b19c69e2bf0
Cesar Li Chen <cesarli619@gmail.com>
2022-06-29 17:31:52 -0400

    Player guess logic added and working

--- guess.py
+++ guess.py
@@ -9,4 +9,18 @@ name = input()
 print(f"Hi {name}, I'm thinking of a number between 1 and 20.")
 secret_number = random.randint(1,20)
 
+# Let player guess up to 6 times
+for guesses in range (1, 7):
+    print("Take a guess.")
+    guess = int(input())
+
+    if guess < secret_number:
+        print("Your guess is too low")
+    elif guess > secret_number:
+        print("Your guess is too high.")
+    else:
+        # When the guess is correct.
+        break
+
+print("You took " + str(guesses) + " guesses.")
 
