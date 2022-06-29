b9bfc6b4e5a4ddcb4f75592b82ee8237c73c4808
Cesar Li Chen <cesarli619@gmail.com>
2022-06-29 17:52:03 -0400

    Added some input validation, emphasis on some

--- guess.py
+++ guess.py
@@ -12,15 +12,22 @@ secret_number = random.randint(1,20)
 # Let player guess up to 6 times
 for guesses in range (1, 7):
     print("Take a guess.")
-    guess = int(input())
+    
+    guess_input = input()
+    guess = 0
+    if guess_input.strip().isdigit():
 
-    if guess < secret_number:
-        print("Your guess is too low")
-    elif guess > secret_number:
-        print("Your guess is too high.")
+        guess = int(guess_input)
+        if guess < secret_number:
+            print("Your guess is too low")
+        elif guess > secret_number:
+            print("Your guess is too high.")
+        else:
+            # When the guess is correct.
+            break
     else:
-        # When the guess is correct.
-        break
+        print("Uuhhmmm... What you typed does not appear to be a number")
+        print("I'm counting that as a guess though")
 
 
 if guess == secret_number:
