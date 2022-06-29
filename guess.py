3a6066f4e452a2dfd059bb81588a96a361d9d0a5
Cesar Li Chen <cesarli619@gmail.com>
2022-06-29 17:25:28 -0400

    Salutations and random number generation working

--- guess.py
+++ guess.py
@@ -1,3 +1,12 @@
 # A little guess the nuber game in python.
 # Also used to get familiar with Git and Github
 
+import random
+
+print("Hello. What is you name?")
+name = input()
+
+print(f"Hi {name}, I'm thinking of a number between 1 and 20.")
+secret_number = random.randint(1,20)
+
+
