dbc2a601733536f9e0305f7e7b013f54d0d415a1
Cesar Li (win10) <cesarli619@gmail.com>
2022-11-29 19:17:40 -0500

    Started lists challenge 2

---
+++ python-challenges/lists_challenge_2.py
@@ -0,0 +1,7 @@
+# Take 10 inputs from the user, figure out how many of them are equal.
+# Numbers can will only be from 1 to 10.
+# Without using conditionals, loops.
+
+numbers = input("Please enter the numbers to count")
+num_list = numbers.split(" ")
+
