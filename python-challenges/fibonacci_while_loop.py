7da885158382e573a67448cc130b0115f3c94ac4
Cesar Li (win10) <cesarli619@gmail.com>
2022-11-29 22:08:24 -0500

    Completed fib seq

--- python-challenges/fibonacci_while_loop.py
+++ python-challenges/fibonacci_while_loop.py
@@ -1,13 +1,15 @@
-FIB_RANGE = 100
+FIB_RANGE = 14
 
-n1 = 0
-n2 = 1
-fib_seq = 0
+n1, n2 = 0, 1
+fib_seq, count = 0, 0
 
 print(n1)
 print(n2)
-while n1 < FIB_RANGE:
+# FIB_RANGE - 2, because we already printed
+# first 2 terms.
+while count < FIB_RANGE - 2:
     fib_seq = n1 + n2
     n1 = n2
     n2 = fib_seq
     print(fib_seq)
+    count += 1
