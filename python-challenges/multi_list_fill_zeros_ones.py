6fcae2f83680f3c7dc5092f5dc656bd35a5d330c
Cesar Li (win10) <cesarli619@gmail.com>
2022-12-03 22:19:37 -0500

    Complete fill zeros and ones challenge

--- python-challenges/multi_list_fill_zeros_ones.py
+++ python-challenges/multi_list_fill_zeros_ones.py
@@ -4,15 +4,32 @@ import random
 
 columns = int(input("How many columns for the list"))
 rows = int(input("How many rows for the list"))
+# Below is a bad way of creating 2d lists
+# l = [[7]*columns]*rows
 
-l = [[7]*columns]*rows
+# Better ways:
+# First, list comprehension
+l = [[4 for i in range(columns)] for j in range(rows)]
+
+#Second,
+# l = []
+# for i in range(rows):
+#     col = []
+#     for j in range(columns):
+#         col.append(0)
+#     l.append(col)
 
 print(l)
 for i in range(columns):
     for j in range(rows):
-        print(i % 2)
         if i % 2 == 0:
             l[j][i] = 1
         else:
             l[j][i] = 0
 print(l)
+
+# ======================================================
+# One line solution
+one_line_list = [[1 if i % 2 == 0 else 0 for i in range(columns)] for j in range(rows)]
+print(f"{one_line_list} one line")
+
