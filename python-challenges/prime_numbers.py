1dc0eaf5ef7630e326863738f1edf0e88f81e7df
Cesar Li (win10) <cesarli619@gmail.com>
2023-01-27 23:23:04 -0500

    Cleans up comments

--- python-challenges/prime_numbers.py
+++ python-challenges/prime_numbers.py
@@ -3,21 +3,15 @@
 
 upper_range = int(input("Please give me maximum range for searching prime numbers"))
 
-# For each number 'num' from 2 to the user's inputted 'upper_range'
 for num in range(2, upper_range+1):
-    isPrime = True
+    isPrime = True  # isPrime starts as True
 
-    """ 
-     Loop from 2 to our current number 'num'
-     A prime number is divisible by 1 and itself ONLY.
-     If the number was successfully divided by anything but
-    1 (which is skipped in our loop) or itself, then it is 
-    not a prime number.
-    """
     for i in range(2, num):
+        # if 'num' is successfully divided by any number that is
+        # not 1 (skipped) or itself, then it is not a prime
+        # number.
         if num % i == 0:
             isPrime = False
-    # Once the second for-loop ends, if isPrime is still true
-    # then we print the number.
+
     if isPrime:
         print(num)
