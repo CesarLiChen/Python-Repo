35811a5994c5fa370803d85395121fa35ea608d2
Cesar2 win10 <cesarli619@gmail.com>
2023-02-24 13:48:26 -0500

    Completes naive solution for Container with Most Water

--- python-challenges/container_with_most_water.py
+++ python-challenges/container_with_most_water.py
@@ -13,3 +13,17 @@
 # Input: height = [1,8,6,2,5,4,8,3,7]
 # Output: 49
 
+in_list = [1, 8, 6, 2, 5, 4, 8, 3]
+
+def max_water_container(height: list[int]) -> int:
+    max_water = 0
+
+    for i in range(len(in_list)):
+        for j in range(len(in_list)):
+            water = min(height[i], height[j]) * abs(i - j)
+            if water > max_water:
+                max_water = water
+    return max_water
+
+
+print(max_water_container(in_list))
