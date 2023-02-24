44b3124df85f417a761020b2759435176e6c317c
Cesar2 win10 <cesarli619@gmail.com>
2023-02-24 13:52:21 -0500

    Playing around with some test cases

--- python-challenges/container_with_most_water.py
+++ python-challenges/container_with_most_water.py
@@ -13,7 +13,7 @@
 # Input: height = [1,8,6,2,5,4,8,3,7]
 # Output: 49
 
-in_list = [1, 8, 6, 2, 5, 4, 8, 3]
+in_list = [1, 8, 6, 2, 5, 4, 8, 3, 7]
 
 def max_water_container(height: list[int]) -> int:
     max_water = 0
