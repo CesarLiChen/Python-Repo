b161dbcf469c39c5e10469b2ca0b2647639a3220
Cesar Li Chen <cesarli619@gmail.com>
2023-03-13 12:38:56 -0400

    Not sure what changed

--- python-challenges/conways_game_of_life.py
+++ python-challenges/conways_game_of_life.py
@@ -1,5 +1,5 @@
 # Conway's Game of Life rules:
-# 1. TURNS OFF - If cell is ON and has <= 2 ON neighbours.
+# 1. TURNS OFF - If cell is ON and has < 2 ON neighbours.
 # 2. REMAINS ON - If cell is ON and 2 OR 3 ON neighbours.
 # 3. TURNS OFF - If cell is ON and > 3 ON neighbours.
 # 4. TURNS ON - If cell is OFF and == 3 ON neighbours.
