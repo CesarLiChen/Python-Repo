ee42088ed9e7ef45e5ed4e17dd5a0c5745207d03
Cesar Li Chen <cesarli619@gmail.com>
2023-03-14 15:26:14 -0400

    Checking how many ON neighbours is working

--- python-challenges/conways_game_of_life.py
+++ python-challenges/conways_game_of_life.py
@@ -16,18 +16,34 @@ game_board_list = [[ON_STATE if random.randint(0, 1) == 0 else OFF_STATE for i i
 
 for row in game_board_list:
     for cell in row:
-        print(cell, end=" ")
+        print(cell, end="")
     print()
 
 for r_index in range(rows):
     for c_index in range(columns):
         on_neighbours = 0
-        ur = r_index - 1 # ur -> up row
+        ur = r_index - 1  # ur -> up row
         dr = r_index + 1
-        lc = c_index - 1 # left column
+        lc = c_index - 1  # left column
         rc = c_index + 1
 
-        if 0 <= ur < rows and 0 <= lc <columns: # make sure up left is not out of bounce
+        if 0 <= ur < rows and 0 <= lc < columns:  # make sure UP-LEFT index are inside list range
             on_neighbours += 1 if game_board_list[ur][lc] == ON_STATE else 0
+        if 0 <= ur < rows:  # checks UP
+            on_neighbours += 1 if game_board_list[ur][c_index] == ON_STATE else 0
+        if 0 <= ur < rows and 0 <= rc < columns:  # checks UP-RIGHT
+            on_neighbours += 1 if game_board_list[ur][rc] == ON_STATE else 0
 
-        print(f"[{r_index}][{c_index}] U-L neighbor's state is: {on_neighbours}")
+        if 0 <= dr < rows and 0 <= lc < columns:  # DOWN-LEFT
+            on_neighbours += 1 if game_board_list[dr][lc] == ON_STATE else 0
+        if 0 <= dr < rows:  # DOWN
+            on_neighbours += 1 if game_board_list[dr][c_index] == ON_STATE else 0
+        if 0 <= dr < rows and 0 <= rc < columns:  # DOWN-RIGHT
+            on_neighbours += 1 if game_board_list[dr][rc] == ON_STATE else 0
+
+        if 0 <= lc < columns:  # LEFT
+            on_neighbours += 1 if game_board_list[r_index][lc] == ON_STATE else 0
+        if 0 <= rc < columns:  # RIGHT
+            on_neighbours += 1 if game_board_list[r_index][rc] == ON_STATE else 0
+
+        print(f"[{r_index}][{c_index}] has: {on_neighbours} ON neighbours")
